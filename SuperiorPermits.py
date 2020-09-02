e9567aa3d51ef63e7dc836306a7e8c6b03f6b68f
tkravits <taylor.kravits@gmail.com>
2020-09-02 16:17:46 -0600

    Doing some address correction work and merging the CAMA address with permit address

--- SuperiorPermits.py
+++ SuperiorPermits.py
@@ -30,6 +30,157 @@ except:
 # print status
 print('Data loading...\n')
 
-df = df.dropna(how='all')
+# convert the first column to a datetime column, put NaT in non-date fields
+df['Date'] = pd.to_datetime(df.iloc[:,0], errors='coerce')
 
-df['Permit_No'] = df.filter(regex='PERMIT #', axis=1)
+# drop any rows that did not convert to a datetime
+df = df.dropna(subset=['Date'])
+
+df['Permit Number'] = pd.DataFrame(df.iloc[:,1])
+df['Permit Applicant'] = pd.DataFrame(df.iloc[:,2])
+df['Address'] = pd.DataFrame(df.iloc[:,3])
+df['Address'] = df['Address'].str.upper()
+df['Description'] = pd.DataFrame(df.iloc[:,6])
+
+# convert the permit value column
+df['Value Total'] = pd.DataFrame(df.iloc[:,7])
+
+# cleans up the dataframe
+df = df[['Date', 'Permit Number', 'Permit Applicant', 'Address', 'Description', 'Value Total']]
+
+# Address cleanup
+df['Address'] = df['Address'].str.replace('SO ', 'S ', regex=True)
+df['Address'] = df['Address'].str.replace('NO ', 'N ', regex=True)
+df['Address'] = df['Address'].str.replace('BLVE', 'BLVD', regex=True)
+df['Address'] = df['Address'].str.replace('WAT', 'WAY', regex=True)
+df['Address'] = df['Address'].str.replace('PK', 'PEAK', regex=True)
+df['Address'] = df['Address'].str.replace('#', '', regex=True)
+
+# Classifies the description into a format that CAMA can understand
+df.loc[df['Description'].str.contains('valve', case=False, na=False), 'SCOPE'] = 'OTH'
+df.loc[df['Description'].str.contains('electric', case=False, na=False), 'SCOPE'] = 'ELM'
+df.loc[df['Description'].str.contains('electrical', case=False, na=False), 'SCOPE'] = 'ELM'
+df.loc[df['Description'].str.contains('boiler', case=False, na=False), 'SCOPE'] = 'OTH'
+df.loc[df['Description'].str.contains('ductless', case=False, na=False), 'SCOPE'] = 'HTG'
+df.loc[df['Description'].str.contains('furnace', case=False, na=False), 'SCOPE'] = 'HTG'
+df.loc[df['Description'].str.contains('heater', case=False, na=False), 'SCOPE'] = 'OTH'
+df.loc[df['Description'].str.contains('/AC', case=False, na=False), 'SCOPE'] = 'AIR'
+df.loc[df['Description'].str.contains('cooler', case=False, na=False), 'SCOPE'] = 'AIR'
+df.loc[df['Description'].str.contains(' AC ', case=False, na=False), 'SCOPE'] = 'AIR'
+df.loc[df['Description'].str.contains('AC ', case=False, na=False), 'SCOPE'] = 'AIR'
+df.loc[df['Description'].str.contains('AC', case=False, na=False), 'SCOPE'] = 'AIR'
+df.loc[df['Description'].str.contains('A/C', case=False, na=False), 'SCOPE'] = 'AIR'
+df.loc[df['Description'].str.contains('PV', case=False, na=False), 'SCOPE'] = 'ENR'
+df.loc[df['Description'].str.contains('solar', case=False, na=False), 'SCOPE'] = 'ENR'
+df.loc[df['Description'].str.contains('photo', case=False, na=False), 'SCOPE'] = 'ENR'
+df.loc[df['Description'].str.contains('P.V.', case=False, na=False), 'SCOPE'] = 'ENR'
+df.loc[df['Description'].str.contains('photovoltaic', case=False, na=False), 'SCOPE'] = 'ENR'
+df.loc[df['Description'].str.contains('geotherm', case=False, na=False), 'SCOPE'] = 'ENR'
+df.loc[df['Description'].str.contains('flush-mounted', case=False, na=False), 'SCOPE'] = 'ENR'
+df.loc[df['Description'].str.contains('Fence', na=False), 'SCOPE'] = 'FEN'
+df.loc[df['Description'].str.contains('Tenant', na=False), 'SCOPE'] = 'TFN'
+df.loc[df['Description'].str.contains('Kitchen', na=False), 'SCOPE'] = 'REM'
+df.loc[df['Description'].str.contains('Remodel', na=False), 'SCOPE'] = 'REM'
+df.loc[df['Description'].str.contains('finished basement', case=False, na=False), 'SCOPE'] = 'BFN'
+df.loc[df['Description'].str.contains('basement finish', case=False, na=False), 'SCOPE'] = 'BFN'
+df.loc[df['Description'].str.contains('basement remodel', case=False, na=False), 'SCOPE'] = 'BFN'
+df.loc[df['Description'].str.contains('bathroom remodel', case=False, na=False), 'SCOPE'] = 'BTH'
+df.loc[df['Description'].str.contains('bath remodel', case=False, na=False), 'SCOPE'] = 'BTH'
+df.loc[df['Description'].str.contains('tenant', case=False, na=False), 'SCOPE'] = 'TFN'
+df.loc[df['Description'].str.contains('New', na=False), 'SCOPE'] = 'NEW'
+df.loc[df['Description'].str.contains('SFR', na=False), 'SCOPE'] = 'NEW'
+df.loc[df['Description'].str.contains('garage', case=False, na=False), 'SCOPE'] = 'GAR'
+df.loc[df['Description'].str.contains('Addition', na=False), 'SCOPE'] = 'ADD'
+df.loc[df['Description'].str.contains('deck', case=False, na=False), 'SCOPE'] = 'DEC'
+df.loc[df['Description'].str.contains('patio', case=False, na=False), 'SCOPE'] = 'POR'
+df.loc[df['Description'].str.contains('porch', case=False, na=False), 'SCOPE'] = 'POR'
+df.loc[df['Description'].str.contains('pergola', case=False, na=False), 'SCOPE'] = 'POR'
+df.loc[df['Description'].str.contains('shed', case=False, na=False), 'SCOPE'] = 'OUT'
+df.loc[df['Description'].str.contains('Wireless', na=False), 'SCOPE'] = 'OTH'
+df.loc[df['Description'].str.contains('Demo', na=False), 'SCOPE'] = 'DEM'
+df.loc[df['Description'].str.contains('Sign', na=False), 'SCOPE'] = 'SGN'
+df.loc[df['Description'].str.contains('Fire', na=False), 'SCOPE'] = 'SPK'
+df.loc[df['Description'].str.contains('Window', na=False), 'SCOPE'] = 'W/D'
+df.loc[df['Description'].str.contains('mobile home', case=False, na=False), 'SCOPE'] = 'MHN'
+df.loc[df['Description'].str.contains('trailer', case=False, na=False), 'SCOPE'] = 'MHN'
+df.loc[(df['Description'].str.contains('Roof', na=False)), 'SCOPE'] = 'RRR'
+df.loc[(df['Description'].str.contains('Roofing', na=False)) & (
+    df['Description'].str.contains('commercial', case=False, na=False)), 'SCOPE'] = 'CRR'
+
+#TODO - could pull the dor code to establish commercial roofing vs residential
+
+print('Establishing connection...\n')
+c_str = open('connection_string.txt', 'r').read()  # can be removed once connection string is added
+cnxn = pyodbc.connect(c_str)
+
+sql = '''SELECT distinct parcel.strap, parcel.status_cd, parcel.dor_cd, parcel.nh_cd FROM r_prod.dbo.parcel
+WHERE (parcel.dor_cd <> 'POSS') AND parcel.status_cd = 'A' '''
+
+sql2 = '''SELECT distinct permit_num FROM r_prod.dbo.permit WHERE permit.agency_id = 'SUP' '''
+
+sql3 = '''SELECT distinct site.strap, site.str_num, site.str_pfx, site.str, site.str_sfx, site.str_sfx_dir,
+site.str_unit FROM r_prod.dbo.site WHERE (site.city IN ('SUPERIOR', 'UNINCORPORATED'))'''
+
+sql4 = '''SELECT distinct parcel.map_id, parcel.nh_cd, parcel.dor_cd, parcel.strap FROM r_prod.dbo.parcel'''
+
+df_active_acct = pd.read_sql(sql, cnxn)
+df_permit = pd.read_sql(sql2, cnxn)
+df_address = pd.read_sql(sql3, cnxn)
+df_spread_for_app = pd.read_sql(sql4, cnxn)
+
+# Takes the permit database, renames the column to Permit Number, and then merges the month's permit with permits found
+# in the database, this makes sure a permit is not double uploaded, or double valuing
+
+df_permit.rename(columns={'permit_num': 'Permit Number'}, inplace=True)
+
+df_uploaded = pd.merge(df, df_permit, on='Permit Number')
+
+# compares permits that are in CAMA vs ones that aren't, merges df and drops ones that are already in CAMA
+df_not_up = df.loc[~df['Permit Number'].isin(df_uploaded['Permit Number'])]
+df_not_up.drop_duplicates()
+
+print('\n\n----- df_not_up -----\n')
+print(df_not_up.head(5))
+
+# make one df that merges active accounts with the address associated with them
+df_active_addr = pd.merge(df_active_acct, df_address, on='strap')
+print('\n\n----- df_active_addr -----\n')
+print(df_active_addr.head(5))
+# print preview
+
+# attempting to take situs address, concat, and compare with the Boulder permit address (only using active accts, no
+# possessory interest)
+
+df_active_addr.dropna(subset=['str_num'])
+df_active_addr['str_num'] = df_active_addr['str_num'].astype(int).astype(str)
+df_active_addr['str_pfx'] = df_active_addr['str_pfx'].fillna(np.nan).replace(np.nan, ' ', regex=True)
+df_active_addr['str_pfx'] = df_active_addr['str_pfx'].replace('  ', ' ', regex=True)
+df_active_addr['str_pfx'] = df_active_addr['str_pfx'].replace('S', ' S', regex=True)
+df_active_addr['str_pfx'] = df_active_addr['str_pfx'].replace('N', ' N', regex=True)
+df_active_addr['str_pfx'] = df_active_addr['str_pfx'].replace('E', ' E', regex=True)
+df_active_addr['str_pfx'] = df_active_addr['str_pfx'].replace('W', ' W', regex=True)
+df_active_addr['str_sfx'] = df_active_addr['str_sfx'].fillna(np.nan).replace(np.nan, ' ', regex=True)
+df_active_addr['str_sfx'] = df_active_addr['str_sfx'].replace('  ', '', regex=True)
+df_active_addr['str_sfx_dir'] = df_active_addr['str_sfx_dir'].fillna(np.nan).replace(np.nan, ' ', regex=True)
+df_active_addr['str_sfx_dir'] = df_active_addr['str_sfx_dir'].replace('  ', ' ', regex=True)
+df_active_addr['str_unit'] = df_active_addr['str_unit'].fillna(np.nan).replace(np.nan, '', regex=True)
+
+# creates a column called Address that is set up in the same format as the Superior permits table
+df_active_addr['Address'] = df_active_addr['str_num'] + df_active_addr['str_pfx'] + df_active_addr['str'] + \
+                            ' ' + df_active_addr['str_sfx'] + df_active_addr['str_sfx_dir'] + df_active_addr['str_unit']
+df_active_addr['Address'] = df_active_addr['Address'].str.rstrip()
+
+# merges the Superior accounts database (strap) with the created Address field with the Superior permit spreadsheet
+df_permit_addr = df.merge(df_active_addr, on='Address', how='left')
+
+# takes the unmerged addresses and makes a spreadsheet to be checked by hand
+df_permit_addr_nostrap = df_permit_addr.loc[df_permit_addr['strap'].isna()]
+df_permit_addr_nostrap.to_excel('Unmerged_superior_permits.xlsx', index=False)
+
+df_final = df_permit_addr.drop(['status_cd', 'dor_cd', 'nh_cd'], axis=1)
+df_final['strap'] = df_final['strap'].str.rstrip()
+df_final.drop_duplicates(subset='Permit Number', keep='first', inplace=True)
+df_final.dropna(subset=['strap'])
+
+df_final = df_permit_addr[["Permit Number", "strap", "Description", "str_num", "str_pfx",
+                     "str", "str_sfx", "str_unit", "Value Total", "Date", "SCOPE"]]

5562063707a3d051f0142b249c0513dfde5f4663
tkravits <taylor.kravits@gmail.com>
2021-01-14 16:40:34 -0700

    second build

--- Louisville_permits.py
+++ Louisville_permits.py
@@ -1,6 +1,7 @@
 import pdfplumber
 import pandas as pd
 import re
+from collections import OrderedDict
 
 df = []
 with pdfplumber.open(r"C:\Users\tkravits\Github\Building-Permit-Automation\Louisville_December2020.pdf") as pdf:
@@ -8,10 +9,8 @@ with pdfplumber.open(r"C:\Users\tkravits\Github\Building-Permit-Automation\Louis
         text = page.extract_text()
         df.append(text)
         str1 = ''.join(df)
-        core_pat = re.compile(r"CONTACTS", re.DOTALL)
-        core = re.search(core_pat, str1).group(0)
+#        core_pat = re.compile(r"CONTACTS", re.DOTALL)
+#        core = re.search(core_pat, str1).group(0)
 
-        # Test out the imagery, might help keep the order to then convert to dataframe
-        im = p0.to_image()
-
-# TODO - Getting the regex pattern to remove the extra spaces
+# TODO - need to use regex to remove the header and footers of the total pdf to make one large list
+# TODO - then I need to group the permits by each new line (aka the permit info and the description)
